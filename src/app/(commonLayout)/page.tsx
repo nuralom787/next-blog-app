f7b4188368cd96cf43dbdf8e56e77c147bbecddc
Nur Alom <md9577630@gmail.com>
2026-01-20 22:40:05 +0600

    update code

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,14 +1,27 @@
 
 import Link from "next/link";
 import { Button } from "../../components/ui/button";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+
+  const cookie = await cookies();
+
+  const res = await fetch("http://localhost:5000/api/auth/get-session", {
+    headers: {
+      cookie: cookie.toString()
+    },
+    cache: "no-store"
+  });
+
+  console.log("SESSION", await res.json());
+
   return (
-    <div className="space-y-3">
+    <div className="space-x-3">
       <h1>Hello World</h1>
       <Button variant={"ghost"}>Click Me</Button>
-      <Link href={"/about"}>About</Link>
-      <Link href={"/dashboard"}>Dashboard</Link>
+      <Link className="" href={"/about"}>About</Link>
+      <Link className="" href={"/dashboard"}>Dashboard</Link>
     </div>
   );
 }
